6316ea569cb2ac4b0b1dee245a0cefd0fc7e3b44
Lucas Henrique <lucas.barbosa3680@gmail.com>
2024-05-05 15:17:26 -0300

    feat: creates Hero component UI

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+import Hero from "./sections/Hero/Hero";
+
+function Home() {
+  return (
+    <>
+    <NavBar />
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
